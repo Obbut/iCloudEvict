59ec099ec7e2c96bf376b208e5bef2be72971e0e
Robbert Brandsma <rbrandsma@e-sites.nl>
2015-06-11 12:40:44 +0200

    Initial Commit

---
+++ iCloudEvict/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  iCloudEvict
+//
+//  Created by Robbert Brandsma on 11-06-15.
+//  Copyright © 2015 Robbert Brandsma. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
