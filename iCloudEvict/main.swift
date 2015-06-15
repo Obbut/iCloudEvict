b3d99a31f0df508c406547094217dea2ce8cf603
Robbert Brandsma <i@robbertbrandsma.nl>
2015-06-15 12:26:26 +0200

    Proof of concept

--- iCloudEvict/main.swift
+++ iCloudEvict/main.swift
@@ -6,7 +6,33 @@
 //  Copyright © 2015 Robbert Brandsma. All rights reserved.
 //
 
-import Foundation
+import AppKit
 
-print("Hello, World!")
+if Process.arguments.count != 3 {
+    fatalError("Usage:\niCloudEvict evict <filepath>\niCloudEvict download <filepath>")
+}
 
+let evict: Bool
+switch Process.arguments[1] {
+case "evict":
+    evict = true
+case "download":
+    evict = false
+default:
+    fatalError("The first argument can be 'evict' or 'download'")
+}
+
+let path = Process.arguments[2]
+
+do {
+    let URL = NSURL(fileURLWithPath: path)
+    if evict {
+        try NSFileManager.defaultManager().evictUbiquitousItemAtURL(URL)
+    } else {
+        try NSFileManager.defaultManager().startDownloadingUbiquitousItemAtURL(URL)
+    }
+    
+    print("Complete.")
+} catch let error {
+    fatalError("The file(s) could not be evicted or downloaded:\n\(error)")
+}
